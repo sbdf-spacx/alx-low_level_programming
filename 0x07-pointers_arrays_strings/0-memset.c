478cffa9f0d46b5b2c5c821b6d0e109516c3d812
Mightyrichard <kingrichard5777@gmail.com>
2022-11-18 00:55:15 -0800

    modified 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,16 +1,20 @@
 #include "main.h"
 /**
- * _memcpy - copies memory area
- * @dest: destination memory area
- * @src: source memory area
- * @n: bytes filled
- * Return: dest
+ *_memset - fill a block of memory with a specific value
+ *@s: starting address of memory to be filled
+ *@b: the desired value
+ *@n: number of bytes to be changed
+ *
+ *Return: changed array with new value for n bytes
  */
-char *_memcpy(char *dest, char *src, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	int i = 0;
 
-	for (i = 0; i < n; i++)
-		*(dest + i) = *(src + i);
-	return (dest);
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
 }
